7a76b78b620bae381e64df064b375a57ff5c88fb
Chirag Goyal <chiraggoyal1910@gmail.com>
2022-08-25 14:29:52 +0530

    Minor changes

--- components/homePage/ActionCard.js
+++ components/homePage/ActionCard.js
@@ -1,23 +1,36 @@
 import styles from "../../styles/homePage/actionCard.module.css";
 
-const ActionCard = ({ heading, subHeading, icon, action = () => {} }) => {
+const ActionCard = ({
+    heading,
+    subHeading,
+    icon,
+    action = () => {},
+    loaded = true,
+}) => {
     return (
         <>
-            <div className={styles.main}>
-                <div className={styles.container}>
-                    <div className={styles.info}>
-                        <h3>{heading}</h3>
-                        <p>{subHeading}</p>
-                    </div>
+            <div
+                className={[
+                    styles.main,
+                    !loaded ? "loading " + styles.loading : undefined,
+                ].join(" ")}
+            >
+                {loaded && (
+                    <div className={styles.container}>
+                        <div className={styles.info}>
+                            <h3>{heading}</h3>
+                            <p>{subHeading}</p>
+                        </div>
 
-                    <button
-                        type="button"
-                        className={styles.icon}
-                        onClick={action}
-                    >
-                        {icon}
-                    </button>
-                </div>
+                        <button
+                            type="button"
+                            className={styles.icon}
+                            onClick={action}
+                        >
+                            {icon}
+                        </button>
+                    </div>
+                )}
             </div>
         </>
     );
