381812fac41bfb7115295f2b1985704ed7568c2b
Chirag Goyal <chiraggoyal1910@gmail.com>
2023-01-10 18:53:40 +0530

    Made landing page unprotected

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import Head from "next/head";
+import { connect } from "react-redux";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Landing from "../components/landingPage/Landing";
 
-const LandingPage = () => {
+const LandingPage = ({ user, userLoaded }) => {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (userLoaded && user) {
+            router.replace({ href: "/home" });
+        }
+    }, [user, userLoaded]);
+
     return (
         <>
             <Head>
@@ -13,4 +24,11 @@ const LandingPage = () => {
     );
 };
 
-export default LandingPage;
+const mapStateToProps = (state) => {
+    return {
+        user: state.auth.user,
+        userLoaded: state.auth.loaded,
+    };
+};
+
+export default connect(mapStateToProps)(LandingPage);
